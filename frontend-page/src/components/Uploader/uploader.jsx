f4d16a6172b3b299f586fecf2da8efc33de136c9
Frederick Nkwonta <frederickn076@gmail.com>
2023-11-14 13:27:41 -0500

    more changes

--- frontend-page/src/components/Uploader/uploader.jsx
+++ frontend-page/src/components/Uploader/uploader.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./uploader.css";
 import { BsFillCloudArrowDownFill } from "react-icons/bs";
-import { FaFileAlt } from "react-icons/fa";
-
-import { useState } from "react";
+import { FaFileAlt, FaCheck } from "react-icons/fa"; // Import FaCheck icon
 
 function Uploader() {
+  const [files, setFiles] = useState([]); // State to keep track of uploaded files
+
   const handleFormClick = () => {
     document.querySelector(".input-field").click();
+  };
+
+  const handleUpload = (e) => {
+    const uploadedFiles = e.target.files;
+    if (uploadedFiles.length === 0) {
+      console.log("No file selected");
+      return;
+    }
+
+    // Convert FileList to an array and update the files state
+    const newFiles = Array.from(uploadedFiles);
+    setFiles((prevFiles) => [...prevFiles, ...newFiles]);
   };
 
   return (
@@ -21,18 +33,29 @@ function Uploader() {
               type="file"
               accept="image/*"
               className="input-field"
+              multiple // Allow multiple file selection
+              onChange={handleUpload}
               hidden
             />
             <BsFillCloudArrowDownFill color="BEDBE8" size={100} />
-            <h3> Drop file your files here. or Browse </h3>
+            <h3> Drop your files here or Browse </h3>
           </form>
         </div>
+
         <div className="wrapper2">
-          <section></section>
+          <section>
+            {files.map((uploadedFile, index) => (
+              <div className="uploaded-file" key={index}>
+                <FaFileAlt color="blue" size={24} />
+                <span>{uploadedFile.name}</span>
+                <FaCheck color="green" size={18} />
+              </div>
+            ))}
+          </section>
         </div>
       </div>
     </main>
   );
-  h;
 }
+
 export default Uploader;
