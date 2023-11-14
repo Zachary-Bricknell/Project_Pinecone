3879b897851b29c727c304070b3d7b999269292d
Frederick Nkwonta <frederickn076@gmail.com>
2023-11-14 11:55:58 -0500

    updates to the front end

--- frontend-page/src/components/Uploader/uploader.jsx
+++ frontend-page/src/components/Uploader/uploader.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./uploader.css";
 import { BsFillCloudArrowDownFill } from "react-icons/bs";
+import { FaFileAlt } from "react-icons/fa";
+
 import { useState } from "react";
 
 function Uploader() {
@@ -10,11 +12,25 @@ function Uploader() {
 
   return (
     <main>
-      <form action="" onClick={handleFormClick}>
-        <input type="file" accept="image/*" className="input-field" hidden />
-        <BsFillCloudArrowDownFill color="BEDBE8" size={100} />
-        <h3> Drop file your files here. or Browse </h3>
-      </form>
+      <nav> </nav>
+      <header> Upload Files </header>
+      <div>
+        <div className="wrapper1">
+          <form action="" onClick={handleFormClick}>
+            <input
+              type="file"
+              accept="image/*"
+              className="input-field"
+              hidden
+            />
+            <BsFillCloudArrowDownFill color="BEDBE8" size={100} />
+            <h3> Drop file your files here. or Browse </h3>
+          </form>
+        </div>
+        <div className="wrapper2">
+          <section></section>
+        </div>
+      </div>
     </main>
   );
   h;
