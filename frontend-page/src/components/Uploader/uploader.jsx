74e53c7c81f2055cdf6cf84d7bdefa9788572096
Frederick Nkwonta <frederickn076@gmail.com>
2023-11-15 17:46:00 -0500

    some css changes

--- frontend-page/src/components/Uploader/uploader.jsx
+++ frontend-page/src/components/Uploader/uploader.jsx
@@ -5,6 +5,7 @@ import { FaFileAlt, FaCheck } from "react-icons/fa"; // Import FaCheck icon
 
 function Uploader() {
   const [files, setFiles] = useState([]); // State to keep track of uploaded files
+  const [dragging, setDragging] = useState(false); // State to track drag events
 
   const handleFormClick = () => {
     document.querySelector(".input-field").click();
@@ -17,9 +18,33 @@ function Uploader() {
       return;
     }
 
-    // Convert FileList to an array and update the files state
-    const newFiles = Array.from(uploadedFiles);
-    setFiles((prevFiles) => [...prevFiles, ...newFiles]);
+    // Update the files state with uploaded files
+    setFiles((prevFiles) => [...prevFiles, ...Array.from(uploadedFiles)]);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setDragging(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragging(false);
+
+    const droppedFiles = e.dataTransfer.files;
+
+    // Update the files state with dropped files
+    setFiles((prevFiles) => [...prevFiles, ...Array.from(droppedFiles)]);
   };
 
   return (
@@ -27,11 +52,16 @@ function Uploader() {
       <nav> </nav>
       <header> Upload Files </header>
       <div>
-        <div className="wrapper1">
+        <div
+          className={`wrapper1 ${dragging ? "dragover" : ""}`}
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+        >
           <form action="" onClick={handleFormClick}>
             <input
               type="file"
-              accept="image/*"
               className="input-field"
               multiple // Allow multiple file selection
               onChange={handleUpload}
@@ -43,7 +73,7 @@ function Uploader() {
         </div>
 
         <div className="wrapper2">
-          <section>
+          <section className="file-section">
             {files.map((uploadedFile, index) => (
               <div className="uploaded-file" key={index}>
                 <FaFileAlt color="blue" size={24} />
