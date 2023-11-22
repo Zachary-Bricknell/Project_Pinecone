739508679da28aa262b0d33017d25901ee067640
Zach <zachary.bricknell@gmail.com>
2023-11-21 20:38:17 -0500

    Updated to only use the first 3 columns of any given filetype, ignoring color or normals

--- backend/utils/file_operations.py
+++ backend/utils/file_operations.py
@@ -1,36 +1,30 @@
-import bson
-import gzip
-import laspy
 import numpy as np
 import open3d as o3d
+import laspy
 
-# Read the original pointcloud and ensure its a supported extension. Return a open3d object. 
-def read_point_cloud(path):    
-    try:        
+def read_point_cloud(path):
+    try:
         file_extension = path.split('.')[-1].lower()
-        
-        if file_extension in ['ply', 'las', 'pcd', 'xyz']:
-            pcd = o3d.io.read_point_cloud(path)
-        
-        # Laz is compressed las and requires an additional step
-        elif file_extension == 'laz':
-            laz_file = laspy.read(path)
-            points = np.vstack((laz_file.x, laz_file.y, laz_file.z)).transpose()
+
+        # Load the point cloud file depending on the file extension
+        if file_extension in ['ply', 'las', 'laz', 'xyz']:
+            # For .laz files, use laspy to read the compressed .las file
+            if file_extension == 'laz':
+                laz_file = laspy.read(path)
+                points = np.vstack((laz_file.x, laz_file.y, laz_file.z)).transpose()
+            else:
+                # For other file types, use numpy.loadtxt to read only the first three columns
+                points = np.loadtxt(path, usecols=(0, 1, 2))
+
+            # Create an Open3D point cloud object with the XYZ data
             pcd = o3d.geometry.PointCloud()
             pcd.points = o3d.utility.Vector3dVector(points)
-            
+            return pcd
+
         else:
-            # Prompt for unsupported filetype
-            # Current filetypes: Open3d compatible, laz, las, xyz
+            # Unsupported file format
             raise ValueError(f"Unsupported file format: {file_extension}")
-        
-        # Check if the point cloud is empty and raise an error
-        if pcd.is_empty():
-            raise ValueError("The point cloud has no points.")
-        
-        return pcd
-    
+
     except Exception as e:
         print(f"Error reading the point cloud: {e}")
         return None
-
