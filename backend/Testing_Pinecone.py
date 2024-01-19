b87ecb417b0668837eff0212772888744cb2e3e6
LukaNikolaisvili <jokerpoker1213@gmail.com>
2024-01-18 21:49:24 -0500

    a

--- backend/Testing_Pinecone.py
+++ backend/Testing_Pinecone.py
@@ -4,7 +4,7 @@ from stages.point_cloud_cleaning_stage import *
 from stages.point_cloud_preprocessing_stage import *
 
 # Reading the point cloud (XYZ format file)
-pointCloudFile = "../var/jenkins_home/filecl4324.xyz"
+pointCloudFile = "../var/jenkins_home/file.xyz"
 point_cloud = o3d.io.read_point_cloud(pointCloudFile)
 
 class TestPointCloudFunctions(unittest.TestCase):
