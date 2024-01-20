611b4ba35595372bdfa71fe755c77a584060f6fd
Frederick Nkwonta <frederickn076@gmail.com>
2024-01-20 09:36:22 -0500

    change rq made my luka

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 import subprocess
 import os
 
+
 def browse_file():
     file_path = filedialog.askopenfilename(
         filetypes=[("XYZ Files", "*.xyz")],
@@ -12,13 +13,21 @@ def browse_file():
         file_path_label.config(text=f"File Path: {file_path}")
         process_button["state"] = "normal"
         visualize_button["state"] = "normal"
-
+        process_and_visualize_button["state"] = "normal"
+        
 def call_backend(file_path, command):
     # Get the absolute path to the backend script
-    backend_script = os.path.abspath(os.path.join("backend", "main.py"))
+    backend_script = os.path.abspath("backend\\main.py")
 
     # Call the backend script with the selected file path and command
     subprocess.run(["python", backend_script, file_path, command])
+
+#new method for processing and visualizing same time
+def call_backend_for_process_visualize(file_path, command1, command2):
+    backend_script = os.path.abspath("backend\\main.py")
+
+    # Call the backend script with the selected file path and command
+    subprocess.run(["python", backend_script, file_path] + command1.split() + command2.split())
 
 def process_file():
     # Call the backend with a process command
@@ -29,10 +38,15 @@ def visualize_file():
     # Call the backend with a visualize command
     file_path = file_path_label.cget("text")[11:]  # Extract file path from label text
     call_backend(file_path, "--visualize")
+    
+def processadnVisualize_file():
+    file_path = file_path_label.cget("text")[11:]
+    call_backend_for_process_visualize(file_path, "--process","--visualize")
+    
 
 root = tk.Tk()
-root.title("Pinecone Project ")
-root.geometry("500x250")
+root.title("Pinecone Project")
+root.geometry("800x600")
 
 # Create a frame for a cleaner layout
 frame = tk.Frame(root, padx=20, pady=20)
@@ -49,5 +63,8 @@ process_button.pack(pady=10)
 
 visualize_button = tk.Button(frame, text="Visualize File", command=visualize_file, font=("Helvetica", 12), state="disabled")
 visualize_button.pack(pady=10)
+
+process_and_visualize_button = tk.Button(frame, text="Process and Visualize File", command=processadnVisualize_file, font=("Helvetica", 12), state="disabled")
+process_and_visualize_button.pack(pady=10)
 
 root.mainloop()
