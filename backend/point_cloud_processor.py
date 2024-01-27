c1e1ef985985ed71462ea7726daecf38be8b43c1
Zach <zachary.bricknell@gmail.com>
2024-01-27 14:10:17 -0500

    update: Reworked backend and frontend to work with pyinstaller to create a functional executable

--- backend/point_cloud_processor.py
+++ backend/point_cloud_processor.py
@@ -1,44 +1,64 @@
-from utils.point_cloud_utils import get_current_step, STAGE_PREFIXES
+from utils.file_operations import modify_filename, setup_logging
+from utils.point_cloud_utils import get_current_step
 from stages.point_cloud_cleaning_stage import cleaning_stage
 from stages.point_cloud_preprocessing_stage import preprocessing_stage
+from utils.config import STAGE_PREFIXES
+import os
+import open3d as o3d
 
 
-def process_point_cloud(filepath, log_path):
+def steps_to_process_point_cloud(filepath, log_path):
     """
     Parameters:
-    filepath (str): The file path of the point cloud to be processed.
+    filepath (str): Path to the point cloud file to be processed.
+    log_path (str): Path to the directory where logs should be stored.
 
     Returns:
-    str: Filepath of the processed point cloud after completing the current stage.
-    None: If the point cloud processing does not complete a stage or if an error occurs.
-
-    Description:
-    Processes a point cloud file through various stages such as cleaning, 
-    preprocessing, and any subsequent stages as defined by the file's current step.
+    str: Path to the processed point cloud file if all stages complete successfully.
+    None: If processing is incomplete due to a stage not completing or an error.
     
-    This function determines the current stage of the point cloud file based on its naming convention.
-    Depending on the stage, it routes the file to the appropriate processing function. The processing
-    starts from the cleaning stage for raw data. Once a stage is completed, the function can be extended
-    to move the file to the next processing stage. If the current stage is 'preprocessing', the respective
-    function needs to be called.
-
-    The function currently handles the cleaning stage and can be extended to handle additional stages as
-    needed. For each stage, the respective function is expected to return a new filepath for the processed 
-    file and a boolean indicating whether all defined steps are completed to indicate completion.
+    Description:
+    Processes a point cloud file sequentially through defined stages.
+    The function identifies the current stage based on the file's naming convention, then applies the appropriate
+    processing functions for each stage in sequence. If a stage completes successfully, the file is prepared for the next stage.
+    Processing halts if a stage fails to complete or an error occurs.
+    
+    Notes:
+    Acts as the driver code for --process, where main.py currently defines all argparse functions
     """
     
-    step_complete = True
     new_filepath = filepath
-    stage, current_step = get_current_step(filepath) 
-    
-    # Raw data starts at the cleaning stage.
-    if stage == 'cleaning':
-        new_filepath, step_complete = cleaning_stage(new_filepath, current_step, STAGE_PREFIXES['cleaning'], log_path)
-        if step_complete:
-            stage = 'preprocessing'
-            
-    if stage == 'preprocessing' and step_complete:
-        new_filepath, step_complete = preprocessing_stage(new_filepath, current_step, STAGE_PREFIXES['preprocessing']) 
+    stage, current_step = get_current_step(filepath)
+    stage_index = next((index for index, (name, _) in enumerate(STAGE_PREFIXES) if name == stage), 0)
 
-    return new_filepath if stage and step_complete else None
+    # Called the base name of the file being processed to easily reference. 
+    log_filename = os.path.splitext(os.path.basename(filepath))[0]
+    setup_logging(log_filename, log_path)
+
+    for index in range(stage_index, len(STAGE_PREFIXES)):
+        step_complete = False  # default for each iteration
+        stage_name, stage_prefix = STAGE_PREFIXES[index]
+
+        try:
+            # Process the point cloud based on the current stage
+            if stage_name == STAGE_PREFIXES[0][0]:
+                new_filepath, step_complete = cleaning_stage(new_filepath, current_step, stage_prefix, log_path)
+            elif stage_name == STAGE_PREFIXES[1][0]:
+                new_filepath, step_complete = preprocessing_stage(new_filepath, current_step, stage_prefix, log_path)
+
+            if not step_complete:
+                print(f"Stage '{stage_name}' did not complete")
+                break
+
+            if index + 1 < len(STAGE_PREFIXES):
+                next_stage_prefix = STAGE_PREFIXES[index + 1][1]
+                new_filepath = modify_filename(new_filepath, next_stage_prefix, "0")
+                current_step = 0
+
+        except Exception as e:
+            # Handle any exceptions during the processing of the current stage
+            print(f"An error occurred during the '{stage_name}' stage: {e}")
+            return None
+        
+    return new_filepath
     
