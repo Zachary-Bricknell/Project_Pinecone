cf0cf11926206cc7722e5ad539ff7616d18e2ef5
LukaNikolaisvili <jokerpoker1213@gmail.com>
2024-03-07 18:07:57 -0500

    I have fixed the issues existing in the code and put everything together the diameter has the issue still when its ran together with everything needs revision

--- backend/stages/point_cloud_processing_stage.py
+++ backend/stages/point_cloud_processing_stage.py
@@ -6,6 +6,7 @@ import logging
 ### Added for log testing, remove when implemented ###
 import os
 import sys
+import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.file_operations import setup_logging
 
@@ -13,133 +14,24 @@ from utils.file_operations import setup_logging
 
 
 
-def analyze_tree(filepath, log_path):
+def calculate_tree_height(filepath):
     """
-    Analyzes a tree point cloud to measure the circumference and diameter at various heights by taking in a point cloud of a cleaned tree taper.
-    
+    Calculate the height of the tree from a point cloud file.
+
     Parameters:
     filepath (str): The file path of the point cloud.
-    
-    Returns:
-    List of tuples: Each tuple contains the height at which the measurement was taken, the circumference, the radius, and the diameter at that height.
-    """
-    setup_logging("cleaning_stage", log_path)     
-
-    point_cloud = o3d.io.read_point_cloud(filepath)
-    
-    z_values = np.asarray(point_cloud.points)[:, 2]
-    base_height = np.min(z_values)  # Accounts for offsets in points not beginning at exactly [0,0,0].
-    highest_point = np.max(z_values)
-    total_height = highest_point - base_height
-    DBH = 1.3  # Standard Diameter Breast Height.
-    increment_height = (total_height - DBH) / 10
-    measurements = []
-    current_height = base_height + DBH
-    
-    for _ in range(9):  # Iterate 9 times to cover the tree above DBH
-        upper_height = current_height + increment_height
-        upper_height = min(upper_height, highest_point)
-
-        sliced_point_cloud = slice_point_cloud(point_cloud, current_height, upper_height)
-
-        circumference, xo, yo, radius = calculate_circumference_of_cylinder(sliced_point_cloud)
-        diameter_at_upper_height = calculate_diameter_at_height(point_cloud, upper_height)
-        measurements.append((upper_height - base_height, circumference, radius, diameter_at_upper_height))
-        
-        # Prepare for next iteration
-        current_height = upper_height
-    
-    return measurements
-
-def fit_circle_to_points(points):
-    """
-    Description:
-    Fits a circle to a set of 2D points using least squares optimization.
-
-    Parameters:
-    points (numpy array): A 2d array points in the form [[x1, y1], [x2, y2], ...]
 
     Returns:
-    tuple: The center(two values) and the radius.
+    float: Height of the tree in meters.
     """
     try:
-        logging.info("Attempting fit_circle_to_points()...")
-        def residuals(params, x, y):
-            # for use in the least_squares algorithm to get the residuals of the distance 
-            # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html#scipy.optimize.least_squares 
-            xo, yo, r = params
-            return np.sqrt((x - xo)**2 + (y - yo)**2) - r
-        # Initialization using averages
-        x_mean = np.mean(points[:, 0])
-        y_mean = np.mean(points[:, 1])
-        initialization = [x_mean, y_mean, np.std(points[:, 0])]
-
-        x = points[:, 0]
-        y = points[:, 1]
-        result = least_squares(residuals, initialization, args=(x, y))
-
-        xo, yo, radius = result.x
-        logging.info(f"Extracted radius {radius} at {xo}, {yo}")
-        return xo, yo, radius
-    except Exception as e:
-        logging.error(f"Failed to radius: {e}")
-        return 
-        
-
-def calculate_circumference_of_cylinder(sliced_point_cloud):
-    """
-    Description:
-    Calculates the circumference of a cylinder by fitting a circle to the projected points on the XY plane.
-
-    Parameters:
-    sliced_point_cloud (open3d.geometry.PointCloud): A point cloud object containing just the cylinder to be calculated.
-    
-    Returns:
-    tuple: The circumference of the fitted cylinder, center(two values), and the radius.
-    """
-    try:
-        logging.info("Attempting caclulate_circumference_of_cylinder()...")
-        # Project the points onto the XY plane
-        points = np.asarray(sliced_point_cloud.points)
-        projected_points = points[:, :2] #Only the x and Y points
-
-        xo, yo, radius = fit_circle_to_points(projected_points)
-
-        circumference = 2 * np.pi * radius
-        logging.info(f"Extracted radius {radius} and circumference{circumference} at {xo}, {yo}")
-        return circumference, xo, yo, radius
-
-    except Exception as e:
-        logging.error(f"Failed to radius: {e}")
-        return 
-
-def slice_point_cloud(point_cloud, lower_height, upper_height):
-    """
-    Slices a point cloud based ont he lower and upper bound
-
-    Parameters:
-    point_cloud (open3d.geometry.PointCloud): The original point cloud.
-    lower_height (float): The lower bound for height.
-    upper_height (float): The upper bound for height.
-
-    Returns:
-    open3d.geometry.PointCloud: A new point cloud containing only the points within the specified height range.
-    """
-    try:
-        logging.info("Attempting slice_point_cloud()...")
+        point_cloud = o3d.io.read_point_cloud(filepath)
         points = np.asarray(point_cloud.points)
-    
-        # Filter based on the height
-        sliced_points = points[(points[:, 2] >= lower_height) & (points[:, 2] <= upper_height)]
-
-        sliced_point_cloud = o3d.geometry.PointCloud()
-        sliced_point_cloud.points = o3d.utility.Vector3dVector(sliced_points)
-    
-        logging.info(f"Sliced the point cloud at {lower_height} and {upper_height}")
-        return sliced_point_cloud
+        tree_height = np.max(points[:, 2]) - np.min(points[:, 2])
+        return round(tree_height, 2)
     except Exception as e:
-        logging.error(f"Failed to slice point cloud: {e}")
-        return 
+        print(f"Failed to calculate tree height: {e}")
+        return None
 
 
 def calculate_diameter_at_height(point_cloud, height):
@@ -155,19 +47,175 @@ def calculate_diameter_at_height(point_cloud, height):
     float: The Diameter.
     """
     try:
-        logging.info("Attempting calculate_diameter_at_height()...")
         # Assuming the tree is upright and Z represents height, slice a thin section around the desired height
         sliced_point_cloud = slice_point_cloud(point_cloud, height - 0.01, height + 0.01)
         if len(sliced_point_cloud.points) == 0:
-            logging.warning("No points found at the specified height. Returning 0 for diameter.")
-            return
+            return None
 
         # Project points onto the XY plane and calculate circumference
         points = np.asarray(sliced_point_cloud.points)[:, :2]
         _, _, radius = fit_circle_to_points(points)
         diameter = 2 * radius
-        logging.info(f"Extracted diameter {diameter} from the point cloud")
         return diameter
     except Exception as e:
-        logging.error(f"Failed to calculate diameter: {e}")
-        return 
+        print(f"Failed to calculate diameter: {e}")
+        return None
+
+def slice_point_cloud(point_cloud, lower_height, upper_height):
+    logging.info("Slicing point cloud between heights {} and {}.".format(lower_height, upper_height))
+    points = np.asarray(point_cloud.points)
+    sliced_points = points[(points[:, 2] >= lower_height) & (points[:, 2] <= upper_height)]
+    if len(sliced_points) == 0:
+        logging.warning("No points found in the specified height range.")
+    sliced_point_cloud = o3d.geometry.PointCloud()
+    sliced_point_cloud.points = o3d.utility.Vector3dVector(sliced_points)
+    return sliced_point_cloud
+
+
+def fit_circle_to_points(points):
+    """
+    Description:
+    Fits a circle to a set of 2D points using least squares optimization.
+
+    Parameters:
+    points (numpy array): A 2d array points in the form [[x1, y1], [x2, y2], ...]
+
+    Returns:
+    tuple: The center(two values) and the radius.
+    """
+    try:
+        def residuals(params, x, y):
+            # for use in the least_squares algorithm to get the residuals of the distance
+            xo, yo, r = params
+            return np.sqrt((x - xo) ** 2 + (y - yo) ** 2) - r
+
+        # Initialization using averages
+        x_mean = np.mean(points[:, 0])
+        y_mean = np.mean(points[:, 1])
+        initialization = [x_mean, y_mean, np.std(points[:, 0])]
+
+        x = points[:, 0]
+        y = points[:, 1]
+        result = least_squares(residuals, initialization, args=(x, y))
+
+        xo, yo, radius = result.x
+        return xo, yo, radius
+    except Exception as e:
+        print(f"Failed to fit circle to points: {e}")
+        return None
+
+def split_thirteen_cookie(filepath, current_step, stage_prefix):
+    """
+    This method calculates the height of the tree from a point cloud.
+    It splits the tree into 13 equal cookies and counts the height after 1.3m from the ground.
+
+    Parameters:
+        filepath (str): The file path of the point cloud.
+
+    Returns:
+        float: Height of the tree in meters.
+    """
+    try:
+        point_cloud = o3d.io.read_point_cloud(filepath)
+        points = np.asarray(point_cloud.points)
+        tree_height = np.max(points[:, 2]) - np.min(points[:, 2])
+
+        # Define heights for the first four cookies
+        cookie_heights = [0.1, 0.5, 0.9, 1.3]
+        num_cookies = 13
+        height_above_ground = 0  # Initialize height above ground starting from 0
+        cookie_count = 1
+
+        recorded_heights = []
+
+        while cookie_count <= num_cookies:
+            if cookie_count <= 4:
+                # Calculate diameter for cookies 1-4
+                diameter = calculate_diameter_at_height(point_cloud, cookie_heights[cookie_count - 1])
+                if diameter is not None:
+                    print(
+                        f"\nDiameter at cookie {cookie_count}: {round(diameter, 2)} meters, cookie located at height {round(cookie_heights[cookie_count - 1], 2)} meters")
+                # Increment height for next cookie
+                height_above_ground = cookie_heights[cookie_count - 1]
+                recorded_heights.append(height_above_ground)  # Recording the heights
+                cookie_count += 1
+
+            else:
+                remaining_height = tree_height - 1.3
+                increment = remaining_height * 0.10  # 10% increment
+                height_above_ground += increment
+
+                # making sure that the height does not exceed total tree height
+                height_above_ground = min(height_above_ground, tree_height)
+
+                # Calculate diameter for cookies 5-13
+                diameter = calculate_diameter_at_height(point_cloud, height_above_ground)
+                if diameter is not None:
+                    print(
+                        f"\nDiameter at cookie {cookie_count}: {round(diameter, 2)} meters, cookie located at height {round(height_above_ground, 2)} meters")
+                recorded_heights.append(height_above_ground)  # Recording the heights
+                cookie_count += 1
+
+        print(f"\ntotal recorded heights: {len(recorded_heights)}")
+
+        return recorded_heights
+    except Exception as e:
+        print(f"Error in split_thirteen_cookie: {e}")
+        return None
+
+
+def processing_stage(filepath, current_step, stage_prefix, log_path=None):
+    
+    print("\nEntering processing stage...")
+    
+    """
+    Parameters:
+    Description:
+    This function processes a point cloud by calculating the height of the tree.
+    It splits the tree into 13 equal segments ("cookies") and calculates the height 
+    after 1.3m from the ground. The function returns the file path of the processed 
+    point cloud and a boolean indicating the success of the operation.
+    """
+    if log_path:
+        log_path = os.path.join(log_path, "processing_log.txt")
+    
+    try:
+        # Start timing the processing stage
+        startTime = time.time()
+
+        # Read point cloud
+        point_cloud = o3d.io.read_point_cloud(filepath)
+        
+        # Calculate tree height
+        tree_height = calculate_tree_height(filepath)
+        tree_diameter = calculate_diameter_at_height(point_cloud, tree_height)
+ 
+        print(f"\nCalculated Tree Height: {tree_height} meters\n")
+        
+        # Split tree into cookies and calculate diameters
+        split_cookies = split_thirteen_cookie(filepath,current_step,stage_prefix)
+        for i, height in enumerate(split_cookies):
+            tree_diameter = calculate_diameter_at_height(point_cloud, height)
+            print(f"Cookie {i+1} height: {round(height, 2)} meters and diameter is {tree_diameter}")
+
+       
+        if log_path is not None:
+            with open(log_path, 'a') as log_file:
+                log_file.write(f"\nCalculated Tree Height: {tree_height} meters\n")
+                log_file.write(f"\nTree diameter and 13 cookies are : {tree_diameter}\n")
+
+        # Processing stage completion time
+        endTime = time.time()
+        totalTime = endTime - startTime
+        print(f"\nTotal time needed for the tree height calculation: {totalTime}\n")
+
+       
+        return filepath, True
+
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+       
+        if log_path is not None:
+            with open(log_path, 'a') as log_file:
+                log_file.write(f"Error during tree height calculation: {e}\n")
+        return filepath, False
